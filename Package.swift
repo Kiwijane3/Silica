2cc0577aa9953c90b62cbdb2a9c8caa05dec3b61
Jane Fraser <janef0421@gmail.com>
2019-06-18 11:33:59 +1200

    Fixed Cairo dependency reference

--- Package.swift
+++ Package.swift
@@ -1,19 +1,25 @@
-// swift-tools-version:5.0.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "Silica",
-	dependencies: [
-		.package(path: "../Cairo")
-	],
+    name: "Silica",
+    products: [
+        .library(
+            name: "Silica",
+            targets: ["Silica"])
+    ],
+    dependencies: [
+       	.package(url: "https://github.com/Kiwijane3/Cairo.git", from: "1.2.4")
+    ],
     targets: [
         .target(
-			name: "Silica",
-			dependencies: [
-				"Cairo"
-			]
-		)
-    ]
+            name: "Silica",
+            dependencies: ["Cairo"]),
+        .testTarget(
+            name: "SilicaTests",
+            dependencies: ["Silica"])
+    ],
+	swiftLanguageVersions: [.v5]
 )
